0f415102885dbad2650b18f1c382b078b9971405
Carter Jernigan <git@carterjernigan.com>
2025-02-09 13:47:28 -0500

    Kotlin 2.1.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,3 @@ plugins {
 rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
     rootProject.the<NodeJsRootExtension>().versions.webpackCli.version = libs.versions.webpack.cli.get()
 }
-
-rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-    rootProject.the<NodeJsRootExtension>().apply {
-        download = project.property("CLOUDFLARE_IS_INSTALL_NODE").toString().toBoolean()
-        nodeVersion = libs.versions.node.get()
-    }
-}
-
-rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-    rootProject.the<YarnRootExtension>().apply {
-        download = project.property("CLOUDFLARE_IS_INSTALL_YARN").toString().toBoolean()
-    }
-}
